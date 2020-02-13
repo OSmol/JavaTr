6354a24ebe22ba72d4fe5cbcc38cd6f57e441b4d
nekoma-02 <kaineko01@gmail.com>
2020-02-13 07:12:13 +0300

    fixed validate methods

--- src/by/javatr/libproject/service/validator/UserValidator.java
+++ src/by/javatr/libproject/service/validator/UserValidator.java
@@ -1,20 +1,11 @@
 package by.javatr.libproject.service.validator;
 
 public class UserValidator {
-    /**
-     * login c ограничением 3-15 символов,
-     * которыми могут быть буквы и цифры,
-     * первый символ обязательно буква
-     */
+
     public static boolean validateLogin(String login) {
         return login.matches("^[a-zA-Z][a-zA-Z0-9]{3,15}$");
     }
 
-    /**
-     * password с ограничением 3-18 символов,
-     * которыми могут быть буквы и цифры,
-     * первый символ обязательно буква
-     */
     public static boolean validatePassword(String password) {
 
         return  password.matches("^[a-zA-Z][a-zA-Z0-9]{3,18}$");
