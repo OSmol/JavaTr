d2ae944b57effa27c39541d2e2541b4071a22554
OSmol <Olga_Smolyakova@outlook.com>
2020-02-17 20:29:15 +0300

    Update Status.java

--- src/by/javatr/libproject/util/Status.java
+++ src/by/javatr/libproject/util/Status.java
@@ -1,5 +1,5 @@
 package by.javatr.libproject.util;
 
-public enum Status {
+public enum Status {// если что-то используется только на одном слое, даже если и код утилитный, то и разместить его лучше на этом слое
     ONLINE, OFFLINE
 }
